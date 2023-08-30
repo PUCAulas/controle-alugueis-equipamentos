2ecf10f6322f650c57ea208fc4037172612f0ab3
duda-amaral <dudaamaralmap3110@gmail.com>
2023-08-30 11:38:19 -0300

    refactoring client

--- codigo/src/br/pucminas/controlealuguel/entidade/Cliente.java
+++ codigo/src/br/pucminas/controlealuguel/entidade/Cliente.java
@@ -9,4 +9,48 @@ public class Cliente {
 	private Integer cpf;
 	private List<Aluguel> listaDeAlugueis;
 
+	public Cliente() {
+
+	}
+
+	public Cliente(String nome, LocalDate dataNascimento, Integer cpf, List<Aluguel> listaDeAlugueis) {
+
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.cpf = cpf;
+		this.listaDeAlugueis = listaDeAlugueis;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Integer getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+
+	public List<Aluguel> getListaDeAlugueis() {
+		return listaDeAlugueis;
+	}
+
+	public void setListaDeAlugueis(List<Aluguel> listaDeAlugueis) {
+		this.listaDeAlugueis = listaDeAlugueis;
+	}
+
 }
