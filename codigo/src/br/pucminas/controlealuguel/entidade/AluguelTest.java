5f42a5a24570a281cc55ee51b72b95162d2c44d6
Guilherme Lage Da Costa <968722@sga.pucminas.br>
2023-09-06 10:59:12 -0300

    test: add junit to classpath

---
+++ codigo/src/br/pucminas/controlealuguel/entidade/AluguelTest.java
@@ -0,0 +1,14 @@
+package br.pucminas.controlealuguel.entidade;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AluguelTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
